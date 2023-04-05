631b92f7e35f0d153c9b1e8edff0df645982474c
Samuel Hierholzer (Adfinis AG) <samuel.hierholzer@adfinis.com>
2023-04-05 08:44:56 +0200

    feat: rename and build flake, aswell as various changes

---
+++ README.md
@@ -0,0 +1,48 @@
+# nix-meddle
+
+Meddle around in your `/nix/store/` without actually destroying anything!
+
+Warning: Really early stage. Except breakages!
+
+[TOC]
+
+# What is this??
+
+Quasi savely meddle around in your nix store.
+
+How often have you thought "men, I just want to reeeaaaaally quickly change
+that one line in that file in the Nix Store".
+But for obvious reasons you don't do it.
+
+Nix-Meddle is here to solve that! Instead of editing files in the nix store,
+this tool copies the files into a save-to-edit path and makes a bind-mount.
+And even better: instead of doing a global mount, it does an `unshare` (some
+kind of shell magic), so that the mount point is only visible for your current
+shell session!
+
+Therefore the global system won't be damaged!
+
+# Install
+
+## Nix Flakes
+
+Just run:
+```bash
+nix run github:Melkor333/nix-meddle
+```
+
+## Nix without flakes
+
+TODO. See the manual install.
+
+(PS: I hated people like me only providing a flake. until I learned flakes.. :) )
+
+# Usage
+
+* Run `nix-meddle`
+* It will ask you some stuff and print out 2 commands at the end
+* Run the `unshare -r -m` command first
+* After that run the `mount...` command
+* Meddle around in the path printed out
+
+TODO: Smol Demo where I replace ls with sl *insert troll face*.
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Python env for shell commander";
+  description = "Meddle with files in the nix store without breaking your system";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,24 +10,34 @@
   flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-
-      # my-python = pkgs.python3Full;
-      # python-with-my-packages = my-python.withPackages (p: with p; [
-      #   requests
-      #   # other python packages you want
-      #   # pygobject3
-      #   # pkgs.networkmanager
-      # ]);
     in {
-    devShell = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        gum
-        rsync
-      ];
-      # shellHook = ''
-      #   PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
-      # '';
-    };
-  });
+      packages = rec {
+        nix-meddle = with import nixpkgs { system = system; };
+        stdenv.mkDerivation {
+        name = "nix-meddle";
+        phases = "installPhase";
+        source = self;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $source/nix-meddle $out/bin/nix-meddle
+          # Hack in the path to binaries
+          sed -i 's|#!/usr/bin/env osh|#!${pkgs.bash}/bin/bash|' $out/bin/nix-meddle
+          sed -i 's|gum |${pkgs.gum}/bin/gum |g' $out/bin/nix-meddle
+          sed -i 's|rsync |${pkgs.rsync}/bin/rsync |g' $out/bin/nix-meddle
+          sed -i 's|"mount"|"${pkgs.util-linux}/bin/mount"|g' $out/bin/nix-meddle
+          '';
+        };
+        default = nix-meddle;
+      };
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gum
+          rsync
+        ];
+        # shellHook = ''
+        #   PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+        # '';
+      };
+    });
 }
 
